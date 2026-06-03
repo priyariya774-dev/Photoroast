a8671dcb47c376e8e769c6ee7851ed34c3009bdc
Priya Riya <priyariya774@gmail.com>
2026-06-03 19:57:56 +0530

    fix railway mysql port

--- server.js
+++ server.js
@@ -50,8 +50,15 @@ secure:process.env.NODE_ENV === "production"
 // MYSQL CONNECTION
 // ======================
 
+// const db = mysql.createConnection({
+// host:process.env.DB_HOST || "localhost",
+// user:process.env.DB_USER || "root",
+// password:process.env.DB_PASSWORD || "",
+// database:process.env.DB_NAME || "my_project"
+// });
 const db = mysql.createConnection({
 host:process.env.DB_HOST || "localhost",
+port:Number(process.env.DB_PORT) || 3306,
 user:process.env.DB_USER || "root",
 password:process.env.DB_PASSWORD || "",
 database:process.env.DB_NAME || "my_project"
